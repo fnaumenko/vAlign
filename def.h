31886e0815aacfbce24580624414de8088bbcd9e
Fedor Naumenko <30726965+fnaumenko@users.noreply.github.com>
2019-07-23 11:31:33 +0100

    Delete def.h

--- def.h
+++
@@ -1,7 +0,0 @@
-#define OPT_DESCF_TSHIFT 3	// number of TABs before decsription field in Options::PrintUsage()
-
-#define _VALIGN
-#define _BEDR_EXT	// extended Read members
-#define _FILE_WRITE
-//#define _NO_ZLIB
-#define _NO_UNODMAP
